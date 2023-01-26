e2fe67c53b685cffb28c7c0a8a481c0ec31b44e5
= <andylackk@outlook.com>
2023-01-26 14:12:08 -0600

    Updates

---
+++ precache-manifest.a486f33c03f62101278441e8caa75a3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "525d41b22e5fc4ebae75",
+    "url": "/robofriends/static/css/main.493cb9e9.chunk.css"
+  },
+  {
+    "revision": "525d41b22e5fc4ebae75",
+    "url": "/robofriends/static/js/main.525d41b2.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/robofriends/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "31e2053598a6704fa5d3",
+    "url": "/robofriends/static/css/2.358fcf29.chunk.css"
+  },
+  {
+    "revision": "31e2053598a6704fa5d3",
+    "url": "/robofriends/static/js/2.31e20535.chunk.js"
+  },
+  {
+    "revision": "52d67bf84ad6447d96d7",
+    "url": "/robofriends/static/js/runtime~main.52d67bf8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3e07875e40321d5e368e91215e265091",
+    "url": "/robofriends/index.html"
+  }
+];
